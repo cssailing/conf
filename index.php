e83bf89b0994b96880df307bdab9dea8b64cfef0
xmlscript <xmlscript@gmail.com>
2015-08-22 13:45:36 +0800

    WebIDE提示说语法错误，而且终端没有php命令？用不惯debian系，等会再试

--- index.php
+++ index.php
@@ -8,6 +8,15 @@
   <h1>test for coding.net PaaS</h1>
   <p>搞啥呢？更新木有体现出来<?='，现在已经支持PHP了'?>。</p>
   <p><?php echo __FILE__?></p>
+  <h2>再改一次，看看PaaS环境的配置和WebIDE有什么不同</h2>
+  <?php
+    echo `uname -a`,PHP_EOL,
+         `pwd`,PHP_EOL,
+         `ll -a`,PHP_EOL,
+         `cat /etc/issue`,PHP_EOL,
+         `hostname`;
+    ?>
+  <hr>
   <pre>
    <?='test'?>
 <?=`pwd`?>
