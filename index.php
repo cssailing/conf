656133f125093e8682add49966adf564de069f6c
xmlscript <xmlscript@gmail.com>
2015-08-22 13:12:52 +0800

    coding.net PaaS 在线演示平台需要用到index.php文件

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset=utf-8>
+  <title>test</title>
+</head>
+<body>
+  <h1>test for coding.net PaaS</h1>
+  <pre>
+    <?=`ll -a`?>
+  </pre>
+</body>
+</html>
